a6f883d2658c585d7bc6df7ee3fe432e4f18b8f8
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-05-10 23:31:33 -0300

    feat: organizing the whole project

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import Menu from "components/menu";
 import Home from "pages/home";
+import Lists from "pages/lists";
 import Score from "pages/score";
 import YouChoose from "pages/youchoose";
-import List from "pages/youchoose/score/list";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function AppRouter() {
@@ -12,7 +12,7 @@ export default function AppRouter() {
                 <Menu />
                 <Routes>
                     <Route path="/home" element={<Home />} />
-                    <Route path="/lists" element={<List />} />
+                    <Route path="/lists" element={<Lists />} />
                     <Route path="/score" element={<Score />} />
                     <Route path="/you-choose" element={<YouChoose />} />
                 </Routes>
