17433f6301e88d635eb340a9f2d71a5b4bd9f1c3
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-05-19 21:23:28 -0300

    feat: create item score

--- src/pages/youchoose/score/index.tsx
+++ src/pages/youchoose/score/index.tsx
@@ -1,13 +1,19 @@
 import Item from "./list/item";
 import StyleScore from "./Score.module.scss";
+import movies from "assets/movies.json";
 
 export default function Score() {
-
+    
     return (
-        <div className={StyleScore.box_score}>
-            <div className={StyleScore.score}>
-                <Item />
+        <>
+            <div className={StyleScore.box_score}>
+                <div className={StyleScore.score}>
+                    <Item id={movies[0].id} poster={movies[0].poster} title={movies[0].title} score={movies[0].score} />
+                    <Item id={movies[1].id} poster={movies[1].poster} title={movies[1].title} score={movies[1].score} />
+                    <Item id={movies[2].id} poster={movies[2].poster} title={movies[2].title} score={movies[2].score} />
+                </div>
             </div>
-        </div>
+        </>
+
     )
 }
--- src/pages/youchoose/score/list/item/index.tsx
+++ src/pages/youchoose/score/list/item/index.tsx
@@ -1,8 +1,29 @@
-export default function Item() {
+import StyleItem from "./Item.module.scss";
+
+interface Props {
+    poster: string;
+    title: string;
+    id: number;
+    score: number;
+}
+
+export default function Item(props: Props) {
+
+    const { id, poster, title, score } = props;
+
+    const myStyle = {
+        backgroundImage: `url(${poster})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center"
+    };
+
     return (
         <>
-            <div>
-                Item
+            <div key={id} className={StyleItem.item}>
+                <img src={poster} alt={title}  style={myStyle} className={StyleItem.item_img} />
+                <p>{title}</p>
+                <div className={StyleItem.item_score}>{score}</div>
             </div>
         </>
     )
