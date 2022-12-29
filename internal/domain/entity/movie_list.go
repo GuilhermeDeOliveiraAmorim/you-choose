b7bd1a1a585f01a6e81917a9fbbc496e011f121d
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-12-28 21:44:31 -0300

    feat(entity): added movie list entity

---
+++ internal/domain/entity/movie_list.go
@@ -0,0 +1,43 @@
+package entity
+
+type MovieList struct {
+	ID          string
+	Title       string
+	Description string
+	Choosers    []*Chooser
+	Movies      []*Movie
+}
+
+func NewMovieList(id, title string, description string) *MovieList {
+	return &MovieList{
+		ID:          id,
+		Title:       title,
+		Description: description,
+	}
+}
+
+func (ml *MovieList) AddChooser(chooser *Chooser) {
+	ml.Choosers = append(ml.Choosers, chooser)
+}
+
+func (ml *MovieList) RemoveChooser(chooser *Chooser) {
+	for i, c := range ml.Choosers {
+		if c.ID == chooser.ID {
+			ml.Choosers = append(ml.Choosers[:i], ml.Choosers[i+1:]...)
+			return
+		}
+	}
+}
+
+func (ml *MovieList) AddMovie(movie *Movie) {
+	ml.Movies = append(ml.Movies, movie)
+}
+
+func (ml *MovieList) RemoveMovie(movie *Movie) {
+	for i, m := range ml.Movies {
+		if m.ID == movie.ID {
+			ml.Movies = append(ml.Movies[:i], ml.Movies[i+1:]...)
+			return
+		}
+	}
+}
