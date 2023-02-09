a473a6c07dbc5ec5dbd19f14e3d2e805891f3c6c
guilhermeamorim <guilhermeamorim@deso-se.com.br>
2023-02-09 16:55:19 -0300

    feat(gin): tratment for methods

--- internal/infra/web/webserver/webserver.go
+++ internal/infra/web/webserver/webserver.go
@@ -11,6 +11,7 @@ type WebServer struct {
 	Router        chi.Router
 	Handlers      map[string]http.HandlerFunc
 	WebServerPort string
+	Verb          string
 }
 
 func NewWebServer(serverPort string) *WebServer {
@@ -21,14 +22,26 @@ func NewWebServer(serverPort string) *WebServer {
 	}
 }
 
-func (s *WebServer) AddHandler(path string, handler http.HandlerFunc) {
+func (s *WebServer) AddHandler(path string, handler http.HandlerFunc, verb string) {
 	s.Handlers[path] = handler
+	s.Verb = verb
 }
 
 func (s *WebServer) Start() {
 	s.Router.Use(middleware.Logger)
 	for path, handler := range s.Handlers {
-		s.Router.Handle(path, handler)
+		switch s.Verb {
+		case "Get":
+			s.Router.Get(path, handler)
+		case "Delete":
+			s.Router.Delete(path, handler)
+		case "Patch":
+			s.Router.Patch(path, handler)
+		case "Put":
+			s.Router.Put(path, handler)
+		case "Post":
+			s.Router.Post(path, handler)
+		}
 	}
 	http.ListenAndServe(s.WebServerPort, s.Router)
 }
