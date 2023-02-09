303b3f7f177788d8a238cde8e674ae414b3d17c6
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2023-02-09 18:31:58 -0300

    bug(webhandler): needs more investigation

--- internal/infra/web/webserver/webserver.go
+++ internal/infra/web/webserver/webserver.go
@@ -11,7 +11,6 @@ type WebServer struct {
 	Router        chi.Router
 	Handlers      map[string]http.HandlerFunc
 	WebServerPort string
-	Verb          string
 }
 
 func NewWebServer(serverPort string) *WebServer {
@@ -22,26 +21,14 @@ func NewWebServer(serverPort string) *WebServer {
 	}
 }
 
-func (s *WebServer) AddHandler(path string, handler http.HandlerFunc, verb string) {
+func (s *WebServer) AddHandler(path string, handler http.HandlerFunc) {
 	s.Handlers[path] = handler
-	s.Verb = verb
 }
 
 func (s *WebServer) Start() {
 	s.Router.Use(middleware.Logger)
 	for path, handler := range s.Handlers {
-		switch s.Verb {
-		case "Get":
-			s.Router.Get(path, handler)
-		case "Delete":
-			s.Router.Delete(path, handler)
-		case "Patch":
-			s.Router.Patch(path, handler)
-		case "Put":
-			s.Router.Put(path, handler)
-		case "Post":
-			s.Router.Post(path, handler)
-		}
+		s.Router.Handle(path, handler)
 	}
 	http.ListenAndServe(s.WebServerPort, s.Router)
 }
