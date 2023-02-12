7bcee388557a960e8e0b77dd97e7f54911a221ec
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2023-02-12 19:06:10 -0300

    feat(usecase): created principals use cases for actors

--- internal/entity/actor_repository_interface.go
+++ internal/entity/actor_repository_interface.go
@@ -1,11 +1,11 @@
 package entity
 
 type ActorRepositoryInterface interface {
-	Create(actor *Actor) error                     //
-	Update(actor *Actor) error                     //
-	Delete(id string) (*Actor, error)              //
-	IsDeleted(id string) error                     //
-	Find(id string) (Actor, error)                 //
-	FindAll() ([]Actor, error)                     //
-	FindAllActorMovies(id string) ([]Movie, error) //
+	Create(actor *Actor) error
+	Find(id string) (Actor, error)
+	Update(actor *Actor) error
+	Delete(id string) (*Actor, error)
+	IsDeleted(id string) error
+	FindAll() ([]Actor, error)
+	FindAllActorMovies(id string) ([]Movie, error)
 }
--- internal/infra/database/actor_repository.go
+++ internal/infra/database/actor_repository.go
@@ -30,31 +30,6 @@ func (actorRepository *ActorRepository) Create(actor *entity.Actor) error {
 	return nil
 }
 
-func (actorRepository *ActorRepository) FindAll() ([]entity.Actor, error) {
-	rows, err := actorRepository.Db.Query("SELECT actor_id, name, picture, is_deleted, created_at, updated_at, deleted_at FROM actors")
-	if err != nil {
-		return nil, err
-	}
-
-	var actors []entity.Actor
-
-	for rows.Next() {
-		var actor entity.Actor
-
-		if err := rows.Scan(&actor.ID, &actor.Name, &actor.Picture, &actor.IsDeleted, &actor.CreatedAt, &actor.UpdatedAt, &actor.DeletedAt); err != nil {
-			return actors, err
-		}
-
-		actors = append(actors, actor)
-	}
-
-	if err = rows.Err(); err != nil {
-		return actors, err
-	}
-
-	return actors, nil
-}
-
 func (actorRepository *ActorRepository) Find(id string) (entity.Actor, error) {
 	var actor entity.Actor
 
@@ -81,4 +56,78 @@ func (actorRepository *ActorRepository) Find(id string) (entity.Actor, error) {
 	}
 
 	return actor, nil
+}
+
+func (actorRepository *ActorRepository) Update(actor *entity.Actor) error {
+	stmt, err := actorRepository.Db.Prepare("UPDATE actors SET name = $1, picture = $2, is_deleted = $3, created_at = $4, updated_at = $5, deleted_at = $6 WHERE id = $7")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(actor.Name, actor.Picture, actor.IsDeleted, actor.CreatedAt, actor.UpdatedAt, actor.DeletedAt, actor.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (actorRepository *ActorRepository) Delete(actor *entity.Actor) error {
+	stmt, err := actorRepository.Db.Prepare("UPDATE actors SET is_deleted = $1, deleted_at = $2 WHERE id = $3")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(actor.IsDeleted, actor.DeletedAt, actor.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (actorRepository *ActorRepository) IsDeleted(id string) error {
+	var actor entity.Actor
+
+	rows, err := actorRepository.Db.Query("SELECT * FROM actors WHERE id = $1", id)
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(&actor.ID, &actor.Name, &actor.Picture, &actor.IsDeleted, &actor.CreatedAt, &actor.UpdatedAt, &actor.DeletedAt); err != nil {
+			return err
+		}
+	}
+
+	if err = rows.Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (actorRepository *ActorRepository) FindAll() ([]entity.Actor, error) {
+	rows, err := actorRepository.Db.Query("SELECT actor_id, name, picture, is_deleted, created_at, updated_at, deleted_at FROM actors")
+	if err != nil {
+		return nil, err
+	}
+
+	var actors []entity.Actor
+
+	for rows.Next() {
+		var actor entity.Actor
+
+		if err := rows.Scan(&actor.ID, &actor.Name, &actor.Picture, &actor.IsDeleted, &actor.CreatedAt, &actor.UpdatedAt, &actor.DeletedAt); err != nil {
+			return actors, err
+		}
+
+		actors = append(actors, actor)
+	}
+
+	if err = rows.Err(); err != nil {
+		return actors, err
+	}
+
+	return actors, nil
 }
