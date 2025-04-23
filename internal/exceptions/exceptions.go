d872698d6766c8a8a1b1ed07bcf31cc86bae9caf
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2025-04-23 16:51:47 -0300

    ♻️ refactor: consolidate HTTP status codes in exceptions package and remove redundant rfc.go file

--- internal/exceptions/exceptions.go
+++ internal/exceptions/exceptions.go
@@ -3,6 +3,18 @@ package exceptions
 import "github.com/gin-gonic/gin"
 
 const (
+	RFC200_CODE = 200
+	RFC204_CODE = 204
+	RFC400_CODE = 400
+	RFC401_CODE = 401
+	RFC403_CODE = 403
+	RFC404_CODE = 404
+	RFC409_CODE = 409
+	RFC500_CODE = 500
+	RFC503_CODE = 503
+)
+
+const (
 	RFC200 = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.3.1"
 	RFC204 = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.3.5"
 	RFC400 = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1"
